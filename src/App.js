9baa61719b977bcfe3026e0b00dbbaeac1ed29b6
Akash Kumar <aksh.iem22@gmail.com>
2020-05-31 22:53:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState , useEffect } from 'react';
 import './App.css';
 
 function App() {
+const [subcription, setSubscription] = useState({
+  
+  "type": "subscribe",
+    "channels": [{ "name": "ticker", "product_ids": ["BTC-USD", "ETH-USD", "LTC-USD"] }]
+});
+
+const [btcPrice, setBtcPrice] = useState("");
+const [ethPrice, setEthPrice] = useState("");
+const [ltcPrice, setLtcPrice] = useState("");
+
+const ws = new WebSocket('wss://ws-feed.pro.coinbase.com');
+
+
+
+const initWebSocket = () => {
+  ws.onopen = () => {
+    ws.send(JSON.stringify(subcription));
+  };
+
+  ws.onmessage = (event) => {
+    const response = JSON.parse(event.data);
+    if(response.product_id === 'BTC-USD')
+      setBtcPrice(response.price);
+      if(response.product_id === 'ETH-USD')
+      setEthPrice(response.price);
+      if(response.product_id === 'LTC-USD')
+      setLtcPrice(response.price);
+    
+  }
+
+  ws.onclose = () => {
+    initWebSocket();
+  };
+};
+
+useEffect(()=>{
+  initWebSocket();
+});
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <h1>Cryptocurrency Ticker</h1>
+    </div>
+      <h1>Bitcoin Price: ${btcPrice}</h1>
+      <h1>Litecoin Price: ${ltcPrice}</h1>
+      <h1>Ethereum Price: ${ethPrice}</h1>
+
     </div>
   );
 }
